6e9cdf174b02d115d9c886baa251ccad4d42864f
Mohammed Shekha <msh@odoo.com>
2026-07-09 00:34:52 +0530

    [IMP] expense_tracker: Note about abortSignal

--- expense_tracker/static/src/screens/expense_by_categories/expense_by_categories.js
+++ expense_tracker/static/src/screens/expense_by_categories/expense_by_categories.js
@@ -15,8 +15,19 @@ export class ExpensesByCategory extends Component {
         this.categories = signal.Array([]);
         this.selectedCategory = signal('all');
         this.state = proxy({ expenses: [] });
+        // MSH: Note: I was trying to implement abortSignal example but could not do it
+        // abortSignal works if component is destroyed while fetch is on flight(it is still not completed) 
+        // Actually I have a scenario where I am doing multiple rpc quickly and want to cancel previous one if new request comes
+        // I can do it with AbortController and if we want to use our own current mechanism then we can use KeepLast
+
+        // MSH: Note: Explain useScope and  abortSignal or async call cancellation on document itself, as we do not use API which supports abortSignal
+        // https://odoo.github.io/owl/documentation/v3/owl/reference/scope.html#cancelling-between-awaits
+        // We can use useScope for adding onDestroy callback, where we can do cleanup
+
+        // MSH: Note: Also coult not implement real world example of untrack in this application
+        // we can explain it on documentation itself: https://odoo.github.io/owl/documentation/v3/owl/reference/effects.html#untrack
+
         asyncComputed(async () => {
-            debugger;
             const res = await this.model.load_category_expenses(this.selectedCategory());
             this.state.expenses = res;
         });
