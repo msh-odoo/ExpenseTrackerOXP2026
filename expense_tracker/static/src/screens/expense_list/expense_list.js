fc1a1d2f6f11e0a6d3c2b43b0175a9b781a52406
Mohammed Shekha <msh@odoo.com>
2026-08-11 23:38:03 +0530

    [Solution] 1.5 Add props and props validation concept

--- expense_tracker/static/src/screens/expense_list/expense_list.js
+++ expense_tracker/static/src/screens/expense_list/expense_list.js
@@ -1,9 +1,12 @@
-import { Component, useProps } from "@odoo/owl";
+import { Component, useProps, t } from "@odoo/owl";
 import { screensRegistry } from "@expense_tracker/registries";
 
 export class PersonalExpenseList extends Component {
     static template = "expense_tracker.PersonalExpenseList";
-    props = useProps();
+    props = useProps({
+        expenses: t.array().optional(),
+        ignoreCreate: t.boolean().optional(),
+    });
 }
 
 screensRegistry.add("ExpenseList", PersonalExpenseList);
