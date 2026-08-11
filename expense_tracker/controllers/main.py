5830d73d0a3b553881d51d03f4b3e4ebb9911d2d
Mohammed Shekha <msh@odoo.com>
2026-08-11 13:13:26 +0530

    [ADD] expense_tracker: Add Expense Tracker Module

---
+++ expense_tracker/controllers/main.py
@@ -0,0 +1,14 @@
+from odoo import http
+from odoo.http import request, route
+from odoo.exceptions import AccessError
+from odoo.tools.misc import hmac
+
+class ExpenseTracker(http.Controller):
+
+    @http.route(['/expense_tracker'], type='http', auth='user')
+    def expense_tracker(self, **kwargs):
+        """
+        Renders the owl ecommerce page
+        """
+        session_info = self.env["ir.http"].session_info()
+        return request.render('expense_tracker.root', {"session_info": session_info})
