1c90ac2f0b73fc916b1161ea0206b763638576a2
Mohammed Shekha <msh@odoo.com>
2026-08-12 00:50:17 +0530

    Reactivity concept in header

--- expense_tracker/static/src/components/header/header.js
+++ expense_tracker/static/src/components/header/header.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, proxy, usePlugin } from "@odoo/owl";
+import { ScreenManagerPlugin } from "@expense_tracker/plugins/screen_manager_plugin";
 
 export class Header extends Component {
     static template = "expense_tracker.header";
+
+    setup() {
+        this.sm = usePlugin(ScreenManagerPlugin);
+        this.state = proxy({ activeMenuItem: "home" });
+    }
+
+    onActivateMenu(ev) {
+        const menuName = ev.currentTarget.getAttribute("data-name");
+        this.state.activeMenuItem = menuName;
+        const screenName = ev.currentTarget.getAttribute("data-screen");
+        this.sm.changeScreen({ screen_name: screenName, props: { ignoreCreate: false } });
+    }
 }
