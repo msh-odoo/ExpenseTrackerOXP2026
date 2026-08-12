01648d532c7cca39365619be3cc41d2c5ba37c6d
Mohammed Shekha <msh@odoo.com>
2026-08-12 15:10:06 +0530

    [Solution] 1.10 Add use of life cycle hooks and other hooks

--- expense_tracker/static/src/components/header/header.js
+++ expense_tracker/static/src/components/header/header.js
@@ -1,8 +1,10 @@
 import { Component, proxy, usePlugin } from "@odoo/owl";
 import { ScreenManagerPlugin } from "@expense_tracker/plugins/screen_manager_plugin";
+import { Time } from "../timer/timer";
 
 export class Header extends Component {
     static template = "expense_tracker.header";
+    static components = { Time };
 
     setup() {
         this.sm = usePlugin(ScreenManagerPlugin);
--- expense_tracker/static/src/screens/expense_form/expense_form.js
+++ expense_tracker/static/src/screens/expense_form/expense_form.js
@@ -1,10 +1,47 @@
-import { Component } from '@odoo/owl';
+import { Component, signal, useListener, useEffect } from '@odoo/owl';
 import { screensRegistry } from "@expense_tracker/registries";
 
 class ExpenseForm extends Component {
     static template = "expense_tracker.ExpenseForm";
+
     setup() {
         this.title = "Expense";
+        this.state = proxy({ data: {}, isValidForm: true });
+        this.footer = signal(null);
+        this.form = signal.ref(HTMLElement);
+        useListener(this.form, "click", this._onBounceEffect.bind(this));
+        onWillStart(async () => {
+            const res = {
+                record: {
+                    id: 1,
+                    name: "Brunch at Hotel Leela",
+                    date: "2024-08-21",
+                    amount: 2000.00,
+                    category_id: [1, "Food"]
+                },
+                categories: [
+                    {id: 1, name: "Food"},
+                    {id: 2, name: "Travel"},
+                    {id: 3, name: "Entertaintment"},
+                ],
+            };
+            this.state.data = res;
+        });
+        this.descriptionInput = signal.ref();
+        useEffect(() => {
+            const el = this.descriptionInput();
+            if (el) {
+                el.focus();
+            }
+        });
+    }
+
+    _onBounceEffect() {
+        const buttonEl = this.footer().querySelector("button");
+        buttonEl.classList.add("bounce");
+        setTimeout(() => {
+            buttonEl.classList.remove("bounce");
+        }, 500);
     }
 }
 
