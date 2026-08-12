dd5c9b2ffe12d7ebe05c9dad5f90ffce8eb85da6
Mohammed Shekha <msh@odoo.com>
2026-08-12 16:55:04 +0530

    [Solution] 1.13 Add additional screen for Category list and form

---
+++ expense_tracker/static/src/screens/expense_categories/expense_categories.js
@@ -0,0 +1,80 @@
+import { Component, proxy, useProps, onWillStart, usePlugin, providePlugins } from "@odoo/owl";
+import { screensRegistry } from "@expense_tracker/registries";
+import { useModel } from "../../model/model";
+
+import { BusPlugin } from "../../plugins/bus_plugin";
+import { ScreenManagerPlugin } from "../../plugins/screen_manager_plugin";
+import { ORMPlugin } from "../../plugins/orm_plugin";
+import { ExpenseTrackerModel } from "../../model/expense_tracker_model";
+
+export class ExpenseCategoriesList extends Component {
+    static template = "expense_tracker.CategoriesList";
+    props = useProps();
+    config = {
+        model: ExpenseTrackerModel,
+    };
+
+    setup() {
+        this.model = useModel();
+        // Doc: https://odoo.github.io/owl/documentation/v3/owl/reference/proxies.html
+        this.state = proxy({ categories: [], selectedCategories: [] });
+        this.modelName = "expense.category";
+        this.checkboxInteraction = false;
+        this.sm = usePlugin(ScreenManagerPlugin);
+
+        onWillStart(async () => {
+            const res = await this.model.load_categories(this.props);
+            this.state.categories = res;
+        });
+    }
+
+    _onClickAddCategory(ev) {
+        this.sm.changeScreen({
+            screen_name: "ExpenseCategoryForm",
+            props: { model: "expense.category", isNew: true },
+        });
+    }
+
+    _onClickCategory(ev) {
+        if (!this.checkboxInteraction && this.state.selectedCategories.length === 0) {
+            this.sm.changeScreen({
+                screen_name: "ExpenseCategoryForm",
+                props: { model: "expense.category", id: ev.currentTarget.getAttribute("data-id") },
+            });
+        }
+        this.checkboxInteraction = false;
+    }
+
+    _onCategorySelection(ev) {
+        this.checkboxInteraction = true;
+        const recordId = parseInt(ev.currentTarget.getAttribute("data-id"));
+        if (ev.currentTarget.checked) {
+            if (!this.state.selectedCategories.includes(recordId)) {
+                this.state.selectedCategories.push(recordId);
+            }
+        } else {
+            this.state.selectedCategories = this.state.selectedCategories.filter(
+                (id) => id !== recordId,
+            );
+        }
+    }
+    deleteCategory(ev) {
+        const recordIds = [...this.state.selectedCategories];
+        this._deleteCategory({ detail: { model: "expense.category", ids: recordIds } });
+    }
+
+    async _deleteCategory(ev) {
+        try {
+            const { model, ids } = ev.detail;
+            await this.model.orm.unlink(model, ids);
+            const options = { model: this.modelName };
+            const res = await this.model.load_categories(options);
+            this.state.categories = res;
+            this.state.selectedCategories = [];
+        } catch (error) {
+            console.error("Error deleting categories:", error);
+        }
+    }
+}
+
+screensRegistry.add("CategoriesList", ExpenseCategoriesList);
---
+++ expense_tracker/static/src/screens/expense_categories/expense_categories_form.js
@@ -0,0 +1,70 @@
+import { Component, proxy, usePlugin, onWillStart } from "@odoo/owl";
+import { useModel } from "../../model/model";
+import { screensRegistry } from "@expense_tracker/registries";
+import { BusPlugin } from "@expense_tracker/plugins/bus_plugin";
+import { ScreenManagerPlugin } from "@expense_tracker/plugins/screen_manager_plugin";
+import { ExpenseTrackerModel } from "../../model/expense_tracker_model";
+import { FormView } from "../../components/formview/formview";
+
+class ExpenseCategoryForm extends Component {
+    static template = "expense_tracker.ExpenseCategoryForm";
+    static components = { FormView };
+    config = {
+        model: ExpenseTrackerModel,
+    };
+
+    setup() {
+        this.model = useModel(this.modelParams);
+        this.state = proxy({
+            data: {
+                record: { name: "", icon: "", description: "" },
+                record_fields: {
+                    name: { string: "Name" },
+                    icon: { string: "Icon" },
+                    description: { string: "Description" },
+                },
+            },
+        });
+        this.title = "Category";
+        this.modelName = "expense.category";
+        this.busPlugin = usePlugin(BusPlugin);
+        this.sm = usePlugin(ScreenManagerPlugin);
+
+        onWillStart(async () => {
+            if (this.props.id) {
+                await this.loadData(this.props.id);
+            }
+        });
+    }
+
+    async loadData(id) {
+        const options = {
+            model: this.modelName,
+            id: id,
+            fields: ["name", "icon", "description"],
+        };
+        const res = await this.model.load_data(options);
+        this.state.data = res;
+    }
+
+    async _onAddCategory(newCategory) {
+        if (newCategory.id) {
+            await this.model.orm.write("expense.category", [newCategory.id], {
+                name: newCategory.name,
+                icon: newCategory.icon,
+                description: newCategory.description,
+            });
+        } else {
+            await this.model.orm.create("expense.category", [
+                {
+                    name: newCategory.name,
+                    icon: newCategory.icon,
+                    description: newCategory.description,
+                },
+            ]);
+        }
+        this.sm.changeScreen({ screen_name: "CategoriesList", props: {} });
+    }
+}
+
+screensRegistry.add("ExpenseCategoryForm", ExpenseCategoryForm);
