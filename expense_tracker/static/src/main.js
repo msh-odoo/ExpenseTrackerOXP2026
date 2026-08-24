2a211909e25c9149af71dbc54937fd550c006ce5
Mohammed Shekha <msh@odoo.com>
2026-08-25 01:17:34 +0530

    [Solution] 1.1 Setting up application and add hello world component

--- expense_tracker/static/src/main.js
+++ expense_tracker/static/src/main.js
@@ -1,1 +1,12 @@
-// Code will come here
+import { App, whenReady } from "@odoo/owl";
+import { ExpenseTracker } from "@expense_tracker/expense_tracker";
+
+whenReady(async () => {
+    if (!document.querySelector(".o_expense_tracker")) {
+        return;
+    }
+
+    const app = new App({});
+    const root = app.createRoot(ExpenseTracker, {});
+    await root.mount(document.body);
+});
