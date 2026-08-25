8444616d2d6421bd5f4565c0cc83d293c501eefb
Mohammed Shekha <msh@odoo.com>
2026-08-25 18:37:26 +0530

    [Solution] 1.2 Add Subcomponent

--- expense_tracker/static/src/expense_tracker.js
+++ expense_tracker/static/src/expense_tracker.js
@@ -1,5 +1,13 @@
 import { Component, xml } from "@odoo/owl";
 
-export class ExpenseTracker extends Component {
+class SubComponent extends Component {
     static template = xml`<h1>Hello World</h1>`;
+}
+
+export class ExpenseTracker extends Component {
+    static template = xml`
+    <div><h1>Expense Tracker App</h1></div>
+    <SubComponent/>
+    `;
+    static components = { SubComponent }
 }
