a5bb544080184009646ca68c9f19284697b40e6a
Mohammed Shekha <msh@odoo.com>
2026-08-25 18:39:20 +0530

    [Solution] 1.3 Application config and render template from file

--- expense_tracker/static/src/expense_tracker.js
+++ expense_tracker/static/src/expense_tracker.js
@@ -1,13 +1,5 @@
 import { Component, xml } from "@odoo/owl";
 
-class SubComponent extends Component {
-    static template = xml`<h1>Hello World</h1>`;
-}
-
 export class ExpenseTracker extends Component {
-    static template = xml`
-    <div><h1>Expense Tracker App</h1></div>
-    <SubComponent/>
-    `;
-    static components = { SubComponent }
+    static template = "expense_tracker.root";
 }
--- expense_tracker/static/src/main.js
+++ expense_tracker/static/src/main.js
@@ -1,4 +1,5 @@
 import { App, whenReady } from "@odoo/owl";
+import { getTemplate } from "@web/core/templates";
 import { ExpenseTracker } from "@expense_tracker/expense_tracker";
 
 whenReady(async () => {
@@ -6,7 +7,47 @@ whenReady(async () => {
         return;
     }
 
-    const app = new App({});
+    const urlParams = new URLSearchParams(window.location.search);
+    const debug = urlParams.get("debug");
+    const app = new App({
+        getTemplate,
+        plugins: [],
+        dev: debug || false,
+        name: ExpenseTracker.constructor.name,
+    });
     const root = app.createRoot(ExpenseTracker, {});
     await root.mount(document.body);
+});
+
+
+/**
+ * This code is iterating over the cause property of an error object to console.error a string
+ * containing the stack trace of the error and any errors that caused it.
+ * @param {Event} ev
+ */
+function logError(ev) {
+    ev.preventDefault();
+    let error = ev?.error || ev.reason;
+
+    if (error.seen) {
+        // If an error causes the mount to crash, Owl will reject the mount promise and throw the
+        // error. Therefore, this if statement prevents the same error from appearing twice.
+        return;
+    }
+    error.seen = true;
+
+    let errorMessage = error.stack;
+    while (error.cause) {
+        errorMessage += "\nCaused by: ";
+        errorMessage += error.cause.stack;
+        error = error.cause;
+    }
+    console.error(errorMessage);
+}
+
+window.addEventListener("error", (ev) => {
+    logError(ev);
+});
+window.addEventListener("unhandledrejection", (ev) => {
+    logError(ev);
 });
