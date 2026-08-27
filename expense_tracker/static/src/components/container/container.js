f0bf2cb84c2a72e6f3e3bd25122cd26498356fe1
Mohammed Shekha <msh@odoo.com>
2026-08-27 17:25:22 +0530

    [Solution] 1.5 Add Personal Expenses List Screen

--- expense_tracker/static/src/components/container/container.js
+++ expense_tracker/static/src/components/container/container.js
@@ -1,5 +1,7 @@
 import { Component } from "@odoo/owl";
+import { PersonalExpenseList } from "@expense_tracker/screens/expense_list/expense_list";
 
 export class Container extends Component {
     static template = "expense_tracker.container";
+    static components = { PersonalExpenseList };
 }
---
+++ expense_tracker/static/src/screens/expense_list/expense_list.js
@@ -0,0 +1,5 @@
+import { Component } from "@odoo/owl";
+
+export class PersonalExpenseList extends Component {
+    static template = "expense_tracker.PersonalExpenseList";
+}
