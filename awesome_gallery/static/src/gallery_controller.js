fd38f908799b0c65629a523cf2101acb8a42fdd6
Mohammed Shekha <msh@odoo.com>
2026-08-30 04:37:46 +0530

    [Solution] 4.2 Use the Layout component

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,8 +1,10 @@
 import { Component, useProps } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 import { standardViewProps } from "@web/views/standard_view_props";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
+    static components = { Layout };
     props = useProps({
         ...standardViewProps,
     });
