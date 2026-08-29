3f9f17cfe02ed01276d21fd21d5d75f14b639358
Mohammed Shekha <msh@odoo.com>
2026-08-29 16:53:31 +0530

    [Solution] 1.20 Add ErrorBoundary and Suspense implementation

--- expense_tracker/static/src/components/container/container.js
+++ expense_tracker/static/src/components/container/container.js
@@ -1,7 +1,8 @@
-import { Component, useProps, t } from "@odoo/owl";
+import { Component, useProps, t, Suspense } from "@odoo/owl";
 
 export class Container extends Component {
     static template = "expense_tracker.container";
+    static components = { Suspense };
     props = useProps({
         mainScreenProps: t.object().optional({}),
         tname: t.string(),
