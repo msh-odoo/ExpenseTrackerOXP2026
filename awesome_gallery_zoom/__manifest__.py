e92ebc03ae8bf7ff3d74de615ae61c7e6a8da8ad
Mohammed Shekha <msh@odoo.com>
2026-08-30 19:13:38 +0530

    [Solution] 5.1 Add awesome_gallery_zoom module to zoom the image, wip module not loaded

---
+++ awesome_gallery_zoom/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Gallery View Zoom",
+    'summary': """
+        Starting module for "Master the Odoo web framework, chapter 5: Create a Zoom of Gallery View"
+    """,
+
+    'description': """
+        Starting module for "Master the Odoo web framework, chapter 5: Create a Zoom Gallery View"
+    """,
+
+    'version': '0.1',
+    'author': 'Odoo S.A.',
+    'application': True,
+    'category': 'Tutorials/AwesomeGallery',
+    'installable': True,
+    'depends': ['web', 'contacts', 'awesome_gallery'],
+    'data': [
+        'views/views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_gallery_zoom/static/src/**/*',
+        ],
+    },
+    'license': 'AGPL-3',
+}
