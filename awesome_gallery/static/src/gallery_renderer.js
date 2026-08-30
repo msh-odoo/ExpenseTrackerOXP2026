7270a84a644bc3baac8e77cb75e7c7fa7a104e05
Mohammed Shekha <msh@odoo.com>
2026-08-30 15:52:43 +0530

    [Solution] 4.7 Display images

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,8 +1,10 @@
 import { Component, useProps, t } from "@odoo/owl";
 import { GalleryModel } from "./gallery_model";
+import { GalleryImage } from "./gallery_image";
 
 export class GalleryRenderer extends Component {
     static template = "awesome_gallery.GalleryRenderer";
+    static components = { GalleryImage };
     props = useProps({
         model: t.instanceOf(GalleryModel),
     });
