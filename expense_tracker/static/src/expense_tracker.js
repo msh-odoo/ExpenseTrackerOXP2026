f1639a5ea7650b296f7bb46396a315a28cb26811
Mohammed Shekha <msh@odoo.com>
2026-09-08 16:50:02 +0530

    [Solution] 1.11 Add additional screen for Dashboard

--- expense_tracker/static/src/expense_tracker.js
+++ expense_tracker/static/src/expense_tracker.js
@@ -2,8 +2,8 @@ import { Component, usePlugin, xml } from "@odoo/owl";
 import { Header } from "./components/header/header";
 import { Container } from "./components/container/container";
 import { screensRegistry } from "@expense_tracker/registries";
-import { PersonalExpenseList } from "./screens/expense_list/expense_list";
 import { ScreenManagerPlugin } from "@expense_tracker/plugins/screen_manager_plugin";
+import { Dashboard } from "./screens/expense_dashboard/expense_dashboard";
 
 export class ExpenseTracker extends Component {
     static template = "expense_tracker.root";
@@ -12,20 +12,6 @@ export class ExpenseTracker extends Component {
     setup() {
         super.setup();
         this.sm = usePlugin(ScreenManagerPlugin);
-        this.state = {
-            expenses: [
-                {
-                    description: "Lunch at Leela Hotel",
-                    date: "2024-06-01",
-                    amount: 2000.0,
-                },
-            ],
-        };
-        this.sm.currentScreenProps = Object.assign({}, this.sm.currentScreenProps, {
-            hasButtons: true,
-            showFooter: false,
-            expenses: this.state.expenses,
-        });
-        this.sm.initCurrentScreen({ name: "PersonalExpenseList", component: PersonalExpenseList });
+        this.sm.initCurrentScreen({ name: "Dashboard", component: Dashboard });
     }
 }
